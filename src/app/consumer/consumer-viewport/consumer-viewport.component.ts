9a97fb67b587e46cb30e89d84f6e5614227737e7
shivaboorla <shivakrishnaboorla@gmail.com>
2020-07-01 19:06:56 +0530

    designed address page

--- src/app/consumer/consumer-viewport/consumer-viewport.component.ts
+++ src/app/consumer/consumer-viewport/consumer-viewport.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-consumer-viewport',
@@ -10,6 +11,11 @@ export class ConsumerViewportComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    //Toggle Click Function
+    $("#menu-toggle").click(function(e) {
+      e.preventDefault();
+      $("#wrapper").toggleClass("toggled");
+    });
   }
 
 }
